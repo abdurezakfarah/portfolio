f1a3ac192f27f4325d435aa3932cf72e47c7d02f
abdurezakfarah <abdurazak.awil1@gmail.com>
2024-09-25 11:32:32 +0300

    chore(sitemap): make sitemap homepage slash trailed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,7 +16,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: siteConfig.url,
+      url: `${siteConfig.url}/`,
       lastModified: home?.lastModified,
       changeFrequency: 'daily',
       priority: 1,
